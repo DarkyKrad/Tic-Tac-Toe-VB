b58d3da9044806655243d5a573ab8389bdd9f22d
John Carlo Alam <104263926+DarkyKrad@users.noreply.github.com>
2022-06-02 19:12:38 +0800

    First Commit

--- Tic-Tac-Toe/Form1.Designer.cs
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -35,17 +35,19 @@
             this.label4 = new System.Windows.Forms.Label();
             this.lblP1 = new System.Windows.Forms.Label();
             this.lblDraw = new System.Windows.Forms.Label();
-            this.lvlP2 = new System.Windows.Forms.Label();
+            this.lblP2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnTL = new System.Windows.Forms.Button();
-            this.btnTC = new System.Windows.Forms.Button();
-            this.btnTR = new System.Windows.Forms.Button();
-            this.btnCL = new System.Windows.Forms.Button();
-            this.btnCC = new System.Windows.Forms.Button();
-            this.btnCR = new System.Windows.Forms.Button();
-            this.btnLL = new System.Windows.Forms.Button();
-            this.btnLC = new System.Windows.Forms.Button();
             this.btnLR = new System.Windows.Forms.Button();
+            this.btnLC = new System.Windows.Forms.Button();
+            this.btnLL = new System.Windows.Forms.Button();
+            this.btnCR = new System.Windows.Forms.Button();
+            this.btnCC = new System.Windows.Forms.Button();
+            this.btnCL = new System.Windows.Forms.Button();
+            this.btnTR = new System.Windows.Forms.Button();
+            this.btnTC = new System.Windows.Forms.Button();
+            this.btnTL = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,28 +65,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(79, 394);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.Size = new System.Drawing.Size(54, 14);
             this.label1.TabIndex = 10;
             this.label1.Text = "Player 1";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.Location = new System.Drawing.Point(199, 394);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.Size = new System.Drawing.Size(38, 14);
             this.label2.TabIndex = 11;
             this.label2.Text = "Draw";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.Location = new System.Drawing.Point(323, 394);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.Size = new System.Drawing.Size(54, 14);
             this.label3.TabIndex = 12;
             this.label3.Text = "Player 2";
             // 
@@ -95,7 +99,6 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(0, 15);
             this.label4.TabIndex = 13;
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // lblP1
             // 
@@ -115,14 +118,14 @@
             this.lblDraw.TabIndex = 15;
             this.lblDraw.Text = "0";
             // 
-            // lvlP2
+            // lblP2
             // 
-            this.lvlP2.AutoSize = true;
-            this.lvlP2.Location = new System.Drawing.Point(338, 409);
-            this.lvlP2.Name = "lvlP2";
-            this.lvlP2.Size = new System.Drawing.Size(13, 15);
-            this.lvlP2.TabIndex = 16;
-            this.lvlP2.Text = "0";
+            this.lblP2.AutoSize = true;
+            this.lblP2.Location = new System.Drawing.Point(338, 409);
+            this.lblP2.Name = "lblP2";
+            this.lblP2.Size = new System.Drawing.Size(13, 15);
+            this.lblP2.TabIndex = 16;
+            this.lblP2.Text = "0";
             // 
             // panel1
             // 
@@ -141,6 +144,94 @@
             this.panel1.Size = new System.Drawing.Size(342, 315);
             this.panel1.TabIndex = 17;
             // 
+            // btnLR
+            // 
+            this.btnLR.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLR.Location = new System.Drawing.Point(225, 212);
+            this.btnLR.Name = "btnLR";
+            this.btnLR.Size = new System.Drawing.Size(98, 94);
+            this.btnLR.TabIndex = 8;
+            this.btnLR.Text = " ";
+            this.btnLR.UseVisualStyleBackColor = true;
+            this.btnLR.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnLC
+            // 
+            this.btnLC.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLC.Location = new System.Drawing.Point(121, 212);
+            this.btnLC.Name = "btnLC";
+            this.btnLC.Size = new System.Drawing.Size(98, 94);
+            this.btnLC.TabIndex = 7;
+            this.btnLC.Text = " ";
+            this.btnLC.UseVisualStyleBackColor = true;
+            this.btnLC.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnLL
+            // 
+            this.btnLL.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLL.Location = new System.Drawing.Point(17, 212);
+            this.btnLL.Name = "btnLL";
+            this.btnLL.Size = new System.Drawing.Size(98, 94);
+            this.btnLL.TabIndex = 6;
+            this.btnLL.Text = " ";
+            this.btnLL.UseVisualStyleBackColor = true;
+            this.btnLL.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnCR
+            // 
+            this.btnCR.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCR.Location = new System.Drawing.Point(225, 115);
+            this.btnCR.Name = "btnCR";
+            this.btnCR.Size = new System.Drawing.Size(98, 94);
+            this.btnCR.TabIndex = 5;
+            this.btnCR.Text = " ";
+            this.btnCR.UseVisualStyleBackColor = true;
+            this.btnCR.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnCC
+            // 
+            this.btnCC.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCC.Location = new System.Drawing.Point(121, 115);
+            this.btnCC.Name = "btnCC";
+            this.btnCC.Size = new System.Drawing.Size(98, 94);
+            this.btnCC.TabIndex = 4;
+            this.btnCC.Text = " ";
+            this.btnCC.UseVisualStyleBackColor = true;
+            this.btnCC.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnCL
+            // 
+            this.btnCL.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCL.Location = new System.Drawing.Point(17, 115);
+            this.btnCL.Name = "btnCL";
+            this.btnCL.Size = new System.Drawing.Size(98, 94);
+            this.btnCL.TabIndex = 3;
+            this.btnCL.Text = " ";
+            this.btnCL.UseVisualStyleBackColor = true;
+            this.btnCL.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnTR
+            // 
+            this.btnTR.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnTR.Location = new System.Drawing.Point(225, 15);
+            this.btnTR.Name = "btnTR";
+            this.btnTR.Size = new System.Drawing.Size(98, 94);
+            this.btnTR.TabIndex = 2;
+            this.btnTR.Text = " ";
+            this.btnTR.UseVisualStyleBackColor = true;
+            this.btnTR.Click += new System.EventHandler(this.button_click);
+            // 
+            // btnTC
+            // 
+            this.btnTC.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnTC.Location = new System.Drawing.Point(121, 15);
+            this.btnTC.Name = "btnTC";
+            this.btnTC.Size = new System.Drawing.Size(98, 94);
+            this.btnTC.TabIndex = 1;
+            this.btnTC.Text = " ";
+            this.btnTC.UseVisualStyleBackColor = true;
+            this.btnTC.Click += new System.EventHandler(this.button_click);
+            // 
             // btnTL
             // 
             this.btnTL.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -150,78 +241,29 @@
             this.btnTL.TabIndex = 0;
             this.btnTL.Text = " ";
             this.btnTL.UseVisualStyleBackColor = true;
+            this.btnTL.Click += new System.EventHandler(this.button_click);
             // 
-            // btnTC
+            // btnReset
             // 
-            this.btnTC.Location = new System.Drawing.Point(121, 15);
-            this.btnTC.Name = "btnTC";
-            this.btnTC.Size = new System.Drawing.Size(98, 94);
-            this.btnTC.TabIndex = 1;
-            this.btnTC.Text = " ";
-            this.btnTC.UseVisualStyleBackColor = true;
+            this.btnReset.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.Location = new System.Drawing.Point(49, 38);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(95, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset Game";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
-            // btnTR
+            // btnNewGame
             // 
-            this.btnTR.Location = new System.Drawing.Point(225, 15);
-            this.btnTR.Name = "btnTR";
-            this.btnTR.Size = new System.Drawing.Size(98, 94);
-            this.btnTR.TabIndex = 2;
-            this.btnTR.Text = " ";
-            this.btnTR.UseVisualStyleBackColor = true;
-            // 
-            // btnCL
-            // 
-            this.btnCL.Location = new System.Drawing.Point(17, 115);
-            this.btnCL.Name = "btnCL";
-            this.btnCL.Size = new System.Drawing.Size(98, 94);
-            this.btnCL.TabIndex = 3;
-            this.btnCL.Text = " ";
-            this.btnCL.UseVisualStyleBackColor = true;
-            // 
-            // btnCC
-            // 
-            this.btnCC.Location = new System.Drawing.Point(121, 115);
-            this.btnCC.Name = "btnCC";
-            this.btnCC.Size = new System.Drawing.Size(98, 94);
-            this.btnCC.TabIndex = 4;
-            this.btnCC.Text = " ";
-            this.btnCC.UseVisualStyleBackColor = true;
-            // 
-            // btnCR
-            // 
-            this.btnCR.Location = new System.Drawing.Point(225, 115);
-            this.btnCR.Name = "btnCR";
-            this.btnCR.Size = new System.Drawing.Size(98, 94);
-            this.btnCR.TabIndex = 5;
-            this.btnCR.Text = " ";
-            this.btnCR.UseVisualStyleBackColor = true;
-            // 
-            // btnLL
-            // 
-            this.btnLL.Location = new System.Drawing.Point(17, 212);
-            this.btnLL.Name = "btnLL";
-            this.btnLL.Size = new System.Drawing.Size(98, 94);
-            this.btnLL.TabIndex = 6;
-            this.btnLL.Text = " ";
-            this.btnLL.UseVisualStyleBackColor = true;
-            // 
-            // btnLC
-            // 
-            this.btnLC.Location = new System.Drawing.Point(121, 212);
-            this.btnLC.Name = "btnLC";
-            this.btnLC.Size = new System.Drawing.Size(98, 94);
-            this.btnLC.TabIndex = 7;
-            this.btnLC.Text = " ";
-            this.btnLC.UseVisualStyleBackColor = true;
-            // 
-            // btnLR
-            // 
-            this.btnLR.Location = new System.Drawing.Point(225, 212);
-            this.btnLR.Name = "btnLR";
-            this.btnLR.Size = new System.Drawing.Size(98, 94);
-            this.btnLR.TabIndex = 8;
-            this.btnLR.Text = " ";
-            this.btnLR.UseVisualStyleBackColor = true;
+            this.btnNewGame.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNewGame.Location = new System.Drawing.Point(159, 38);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(95, 23);
+            this.btnNewGame.TabIndex = 18;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
             // 
             // Form1
             // 
@@ -229,7 +271,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(440, 451);
             this.ControlBox = false;
-            this.Controls.Add(this.lvlP2);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblP2);
             this.Controls.Add(this.lblDraw);
             this.Controls.Add(this.lblP1);
             this.Controls.Add(this.label4);
@@ -258,7 +302,7 @@
         private Label label4;
         private Label lblP1;
         private Label lblDraw;
-        private Label lvlP2;
+        private Label lblP2;
         private Panel panel1;
         private Button btnLR;
         private Button btnLC;
@@ -269,5 +313,7 @@
         private Button btnTR;
         private Button btnTC;
         private Button btnTL;
+        private Button btnReset;
+        private Button btnNewGame;
     }
 }
--- Tic-Tac-Toe/Form1.cs
+++ Tic-Tac-Toe/Form1.cs
@@ -2,6 +2,8 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+        bool turn = true;
+        int turn_count = 0;
         public Form1()
         {
             InitializeComponent();
@@ -17,24 +19,145 @@ namespace Tic_Tac_Toe
             this.Close();
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
+        private void btnTL_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void button_click(object sender, EventArgs e)
         {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            winchecker();
+        }
+        private void winchecker()
+        {
+            bool winner = false;
+            if ((btnTL.Text == btnTC.Text && btnTC.Text == btnTR.Text && !btnTL.Enabled))
+                winner = true;
+            else if ((btnCL.Text == btnCC.Text && btnCC.Text == btnCR.Text && !btnCL.Enabled))
+                winner = true;
+            else if ((btnLL.Text == btnLC.Text && btnLC.Text == btnLR.Text && !btnLL.Enabled))
+                winner = true;
+
+            if ((btnTL.Text == btnCL.Text && btnCL.Text == btnLL.Text && !btnTL.Enabled))
+                winner = true;
+            else if ((btnTC.Text == btnCC.Text && btnCC.Text == btnLC.Text && !btnTC.Enabled))
+                winner = true;
+            else if ((btnTR.Text == btnCR.Text && btnCR.Text == btnLR.Text && !btnTR.Enabled))
+                winner = true;
+
+            if ((btnTL.Text == btnCC.Text && btnCC.Text == btnLR.Text && !btnTL.Enabled))
+                winner = true;
+            else if ((btnTR.Text == btnCC.Text && btnCC.Text == btnLL.Text && !btnTR.Enabled))
+                winner = true;
+
+            if (winner)
+            {
+                string win = "";
+                if (turn)
+                {
+                    win = "O";
+                    lblP2.Text = (int.Parse(lblP2.Text) + 1).ToString();
+
+
+                }
+                else
+                {
+                    win = "X";
+                    lblP1.Text = (int.Parse(lblP1.Text) +1).ToString();
+                }
+                btnTL.Enabled = false;
+                btnTC.Enabled = false;
+                btnTR.Enabled = false;
+                btnCL.Enabled = false;
+                btnCC.Enabled = false;
+                btnCR.Enabled = false;
+                btnLL.Enabled = false;
+                btnLC.Enabled = false;
+                btnLR.Enabled = false;
+                MessageBox.Show(win + " Wins!" + " Yay!");
+            }
+            else
+            {
+                if (turn_count == 9)
+                {
+                    MessageBox.Show("Draw!");
+                    lblDraw.Text = (int.Parse(lblDraw.Text) + 1).ToString();
+                }
+            }
+            
+        }
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+
+            btnTL.Enabled = true;
+            btnTC.Enabled = true;
+            btnTR.Enabled = true;
+            btnCL.Enabled = true;
+            btnCC.Enabled = true;
+            btnCR.Enabled = true;
+            btnLL.Enabled = true;
+            btnLC.Enabled = true;
+            btnLR.Enabled = true;
+
+                btnTL.Text = "";
+                btnTC.Text = "";
+                btnTR.Text = "";
+                btnCL.Text = "";
+                btnCC.Text = "";
+                btnCR.Text = "";
+                btnLL.Text = "";
+                btnLC.Text = "";
+                btnLR.Text = "";
+
+            lblP1.Text = "0";
+            lblP2.Text = "0";
+            lblDraw.Text = "0";
+           
+
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnNewGame_Click(object sender, EventArgs e)
         {
+            turn = true;
+            turn_count = 0;
 
-        }
+            btnTL.Enabled = true;
+            btnTC.Enabled = true;
+            btnTR.Enabled = true;
+            btnCL.Enabled = true;
+            btnCC.Enabled = true;
+            btnCR.Enabled = true;
+            btnLL.Enabled = true;
+            btnLC.Enabled = true;
+            btnLR.Enabled = true;
 
-        private void button8_Click(object sender, EventArgs e)
-        {
+            btnTL.Text = "";
+            btnTC.Text = "";
+            btnTR.Text = "";
+            btnCL.Text = "";
+            btnCC.Text = "";
+            btnCR.Text = "";
+            btnLL.Text = "";
+            btnLC.Text = "";
+            btnLR.Text = "";
 
+            
         }
     }
 }
